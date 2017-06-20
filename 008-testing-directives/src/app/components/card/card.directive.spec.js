d48a067d1cf24ad11b24423b82c6b47521c20fc1
Diego Muñoz <godie416@hotmail.com>
2017-06-20 16:56:56 -0500

    Solving issues

--- 008-testing-directives/src/app/components/card/card.directive.spec.js
+++ 008-testing-directives/src/app/components/card/card.directive.spec.js
@@ -37,39 +37,54 @@
                 '</card>';
         }));
 
-        it('Test without parameters', function() {
-            element = $compile(angular.element(html))($rootScope);
-            $rootScope.$digest();
-            expect(element.find('.reservedBy').text()).toEqual($rootScope.card.reservedBy);
+        describe('Test without parameters', function() {
+            it('Should take only the reservedBy', function() {
+                element = $compile(angular.element(html))($rootScope);
+                $rootScope.$digest();
+                expect(element.find('.reservedBy').text()).toEqual($rootScope.card.reservedBy);
+            });
         });
 
-        it('when render, no errors has to show', function() {
-            $rootScope.card = {
-                title: 'Juan Diego',
-                description: 'Talod Digitial',
-                background: 'rgb(77, 77, 77)',
-                textColor: 'rgb(55, 255, 55)',
-                icon: 'account_circle',
-                favorite: function(title) {
-                    return title;
-                },
-                reservedBy: '© Talos Business Card'
-            };
-            element = $compile(angular.element(html))($rootScope);
-            $rootScope.$digest();
+        describe('Test with parameters', function() {
+            beforeEach(function() {
+                $rootScope.card = {
+                    title: 'Juan Diego',
+                    description: 'Talos Digitial',
+                    background: 'rgb(77, 77, 77)',
+                    textColor: 'rgb(55, 255, 55)',
+                    icon: 'account_circle',
+                    favorite: function(title) {
+                        return title;
+                    },
+                    reservedBy: '© Talos Business Card'
+                };
+                element = $compile(angular.element(html))($rootScope);
+                $rootScope.$digest();
+            });
 
-            spyOn($rootScope.card, 'favorite');
+            it('Should set title', function() {
+                expect(element.find('.title').text()).toEqual($rootScope.card.title);
+            });
 
-            expect(element.find('.title').text()).toEqual($rootScope.card.title);
-            expect(element.find('.description').text()).toEqual($rootScope.card.description);
-            expect(element.find('.icon').text()).toEqual($rootScope.card.icon);
-            //Wait until the style is set
-            $timeout(function() {
-                expect(element.find('.card').css('background-color')).toEqual($rootScope.card.background);
-                expect(element.find('.card').css('color')).toEqual($rootScope.card.textColor);
-            }, 100);
-            element.find('.favorite').click();
-            expect($rootScope.card.favorite).toHaveBeenCalledWith($rootScope.card.title);
+            it('Should set description', function() {
+                expect(element.find('.description').text()).toEqual($rootScope.card.description);
+            });
+
+            it('Should set icon', function() {
+                expect(element.find('.icon').text()).toEqual($rootScope.card.icon);
+            });
+
+            it('Should set style', function() {
+                expect(element.css('background-color')).toEqual($rootScope.card.background);
+                expect(element.css('color')).toEqual($rootScope.card.textColor);
+            });
+
+            it('Should add a favorite', function() {
+                spyOn($rootScope.card, 'favorite');
+                element.find('.favorite').click();
+                expect($rootScope.card.favorite).toHaveBeenCalledWith($rootScope.card.title);
+            });
+
         });
     });
 })();
